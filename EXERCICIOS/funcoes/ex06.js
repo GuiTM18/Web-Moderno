63fcdf875466aa2f61f108c5c7451c6ea2235395
GuiTM18 <gui.soft18@gmail.com>
2023-10-30 21:22:19 -0300

    exerc

---
+++ EXERCICIOS/funcoes/ex06.js
@@ -0,0 +1,15 @@
+//CUMPRIMENTAR
+function cumprimetar(nome){
+    console.log('Olá, ' + nome)
+}
+
+cumprimetar('Leonardo')
+cumprimetar('Maria')
+
+
+//ANO EM DIAS
+function anoEmDia(ano){
+    return 365 * ano
+}
+
+console.log(anoEmDia(25))
