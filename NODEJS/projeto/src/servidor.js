dc127144c2cfa394419ee1782068b72912c1f8a5
GuiTM18 <gui.soft18@gmail.com>
2023-11-08 00:48:04 -0300

    api

--- NODEJS/projeto/src/servidor.js
+++ NODEJS/projeto/src/servidor.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 3003
+const bodyParser = require('body-parser');
+const banco = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/produtos', (req, res, next) => {
-    res.send({ nome: 'Iphone 15', valor: 8000})
+    res.send(banco.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(banco.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = banco.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
 })
 
 app.listen(port, () =>{
